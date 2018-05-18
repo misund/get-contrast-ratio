6c3ce7739daaaedd25611adc81e144ef87ef8aa3
misund <thomas@hemmeligadresse.com>
2018-05-18 14:19:45 +0200

    Add first tests

--- test/test.js
+++ test/test.js
@@ -1,9 +1,21 @@
+/* global describe, it */
 import assert from 'assert';
+import getContrastRatio from '../src';
 
-describe('Your package goes here', () => {
-    describe('Your test group goes here', () => {
-        it('should have tests', () => {
-            assert.equal(true, true);
-        });
+describe('Get contrast ratio', () => {
+  describe('Some reference values', () => {
+    it('should return 21 for black on white', () => {
+      assert.equal(getContrastRatio('black', 'white'), 21);
     });
+    it('should return 0 for black on black', () => {
+      assert.equal(getContrastRatio('black', 'white'), 0);
+    });
+    it('should return 0 for white on white', () => {
+      assert.equal(getContrastRatio('black', 'white'), 0);
+    });
+
+    it('should return 0 for gray on gray', () => {
+      assert.equal(getContrastRatio('#444', '#444444'), 0);
+    });
+  });
 });
