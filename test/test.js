a9a0476266ee44bd88db8a852132eb48d1758272
misund <thomas@hemmeligadresse.com>
2018-05-18 13:46:03 +0200

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,9 @@
+import assert from 'assert';
+
+describe('Your package goes here', () => {
+    describe('Your test group goes here', () => {
+        it('should have tests', () => {
+            assert.equal(true, true);
+        });
+    });
+});
