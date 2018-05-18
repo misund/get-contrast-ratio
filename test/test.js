61335a34e1b5a93ca140edb0e550ac33a7c6cd9e
misund <thomas@hemmeligadresse.com>
2018-05-18 14:25:19 +0200

    Fix tests

--- test/test.js
+++ test/test.js
@@ -7,15 +7,15 @@ describe('Get contrast ratio', () => {
     it('should return 21 for black on white', () => {
       assert.equal(getContrastRatio('black', 'white'), 21);
     });
-    it('should return 0 for black on black', () => {
-      assert.equal(getContrastRatio('black', 'white'), 0);
+    it('should return 1 for black on black', () => {
+      assert.equal(getContrastRatio('black', 'black'), 1);
     });
-    it('should return 0 for white on white', () => {
-      assert.equal(getContrastRatio('black', 'white'), 0);
+    it('should return 1 for white on white', () => {
+      assert.equal(getContrastRatio('white', 'white'), 1);
     });
 
-    it('should return 0 for gray on gray', () => {
-      assert.equal(getContrastRatio('#444', '#444444'), 0);
+    it('should return 1 for gray on gray', () => {
+      assert.equal(getContrastRatio('#444', '#444444'), 1);
     });
   });
 });
