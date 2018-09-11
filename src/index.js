6a185eb15406c889f38d1f1b7b9b612f9b51ca70
Nick Schot <nickschot@gmail.com>
2018-09-11 15:10:37 +0200

    fix: return contrast as float with 2 decimals
    Before `Math.floor(constrast, -2)` was used. I think the intended effect was to round the contrast to two decimals. Unfortunately this doesn't work. The new implementation should be correct.

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const getContrastRatio = (color1, color2) => {
 
   const contrast = (lightest + 0.05) / (darkest + 0.05);
 
-  return Math.floor(contrast, -2);
+  return parseFloat(contrast.toFixed(2));
 };
 
 export default getContrastRatio;
