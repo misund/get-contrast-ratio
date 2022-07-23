23b66f422cef0a3411215ac78671ad24e5beecef
misund <thomas@hemmeligadresse.com>
2022-07-23 02:34:40 +0200

    BREAKING: Refactor to typescript
    There shouldn't be any functional changes, but this is a breaking change
    because of the forsaken node versions.
    
    - BREAKING: Stop testing on outdated node versions (prev: 8,10,11 - new: 14,16,18)
    - refactor: Rewrite to typescript
    - build: Use tsc instead of babel
    - build: Upgrade dependencies
    - ci: Move from travis to github actions

---
+++ test/test.ts
@@ -0,0 +1,23 @@
+/* global describe, it */
+import assert from 'assert'
+import getContrastRatio from '../src'
+
+describe('Get contrast ratio', () => {
+  describe('Some reference values', () => {
+    it('should return 21 for black on white', () => {
+      assert.equal(getContrastRatio('black', 'white'), 21)
+    })
+    it('should return 1 for black on black', () => {
+      assert.equal(getContrastRatio('black', 'black'), 1)
+    })
+    it('should return 1 for white on white', () => {
+      assert.equal(getContrastRatio('white', 'white'), 1)
+    })
+    it('should return 1 for gray on gray', () => {
+      assert.equal(getContrastRatio('#444', '#444444'), 1)
+    })
+    it('should return 2.14 for blue on red', () => {
+      assert.equal(getContrastRatio('blue', 'red'), 2.14)
+    })
+  })
+})
